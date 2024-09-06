2ba409f6db9aabd69fd5c7265e1e88835246b574
ZaganTD <177454045+ZaganTD@users.noreply.github.com>
2024-09-06 05:24:24 -0700

    Human choice completed...probably

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rock-paper-scissors
+
+Nothing special to say, it's a simple javascript project to create a rock paper scissor game that will be played in the console.
--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 function getComputerChoice() {
     let choice = Math.random()
     if (choice < 0.33) {
-        console.log('Rock!');
+        return ('Rock!');
     } else if (choice >= 0.33 && choice <= 0.66) {
-        console.log('Paper!');
+        return ('Paper!');
     } else if (choice > 0.66) {
-        console.log('Scissor!');
+        return ('Scissor!');
     }
 }
+ 
+let input = prompt('Rock, Paper, or Scissor?')
+
+function getHumanChoice() {
+    let input2 =  input.toLowerCase();
+    switch (input2) {
+        case 'rock':
+            return 'rock'
+            break;
+        case 'paper':
+            return 'paper'
+            break;
+        case 'scissor':
+            return 'scissor'
+            break;
+    }
+
+}
+
 
 
