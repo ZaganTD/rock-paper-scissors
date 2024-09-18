09142987665c8231b98dec988d2c2e314b48cf81
ZaganTD <177454045+ZaganTD@users.noreply.github.com>
2024-09-18 06:38:08 -0700

    It's not perfect but it's done, i don't know how to modify it to make it better

--- script.js
+++ script.js
@@ -1,31 +1,62 @@
+const a = 'rock'
+const b = 'paper'
+const c = 'scissor'
+
 function getComputerChoice() {
-    let choice = Math.random()
-    if (choice < 0.33) {
-        return ('Rock!');
-    } else if (choice >= 0.33 && choice <= 0.66) {
-        return ('Paper!');
-    } else if (choice > 0.66) {
-        return ('Scissor!');
+    let result = Math.ceil(Math.random() * 3)
+    if (result == 1) {
+        return a
+    } else if (result == 2) {
+        return b
+    } else {
+        return c
     }
 }
- 
-let input = prompt('Rock, Paper, or Scissor?')
 
 function getHumanChoice() {
-    let input2 =  input.toLowerCase();
-    switch (input2) {
-        case 'rock':
-            return 'rock'
+    let input = prompt('Choose your weapon:')
+    let input2 = input.toLowerCase()
+    switch (input2){
+        case a:
+            return a
             break;
-        case 'paper':
-            return 'paper'
+        case b:
+            return b
             break;
-        case 'scissor':
-            return 'scissor'
+        case c:
+            return c
             break;
+        default:
+            alert('Invalid choice!')
     }
 
 }
 
 
+function playGame() {
+    let humanScore = 0
+    let computerScore = 0
 
+function playRound(para1, para2) {
+    if (para1 == para2){
+        alert(`It's a tie, ${para1} and ${para2} are equal!`)
+    } else if ((para1 == a && para2 == c) || (para1 == b && para2 == a) || (para1 == c && para2 == b)) {
+        alert(`You win! ${para1} beats ${para2}!`)
+        console.log(`Your score = ${++humanScore}`)
+    } else if ((para1 == c && para2 == a) || (para1 == a && para2 == b) || (para1 == b && para2 == c)) {
+        alert(`You lose! ${para2} beats ${para1}!`)
+        console.log(`Computer score = ${++computerScore}`)
+    } else {console.log(`you fucked up`)}
+}
+// the first else if statement is the condition for para1 (the player) to win, and the second is the reverse, for the computer to win
+
+playRound(getHumanChoice(), getComputerChoice());
+playRound(getHumanChoice(), getComputerChoice());
+playRound(getHumanChoice(), getComputerChoice());
+playRound(getHumanChoice(), getComputerChoice());
+playRound(getHumanChoice(), getComputerChoice());
+
+
+}
+
+playGame();
